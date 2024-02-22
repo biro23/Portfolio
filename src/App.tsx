41848e131a294c781660b735f5f76a29d217adb4
biro23 <121694408+biro23@users.noreply.github.com>
2024-02-22 10:40:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// deixar apenas o componente aqui
+import Teste from "./Teste"
+
+function App() {
+  return (
+    <Teste />
+  )
+}
+
+export default App
---
+++ src/Teste.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Botao = styled.button `
+background-color: red;
+`;
+
+// criando um novo componente
+
+function Teste () {
+return <Botao >Clique aqui</Botao>
+}
+
+export default Teste
